9a4372489c8f8b9c859031822447ef6dcc2edd20
Chaitanya <chaitanyapatil1311@gmail.com>
2025-12-25 15:36:58 +0530

    Added new Visa class file

---
+++ src/main/java/com/example/booking_ms/visa.java
@@ -0,0 +1,13 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class visa {
+
+    @GetMapping("/visa")
+    public String getData() {
+        return "Click on Visa";
+    }
+}
