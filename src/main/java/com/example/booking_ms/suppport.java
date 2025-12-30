f66a2b92bab940c0b260a90b6520ee3445613de7
Chaitanya <chaitanyapatil1311@gmail.com>
2025-12-30 14:07:46 +0530

    hello team

---
+++ src/main/java/com/example/booking_ms/suppport.java
@@ -0,0 +1,14 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class suppport {
+
+    @GetMapping("/support")
+    public String getData() {
+        return " Welcome support team ";
+
+    }
+}
